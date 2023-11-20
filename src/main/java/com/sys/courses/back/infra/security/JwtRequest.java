4185f03ba9804c7f29f5aa49eb9aab4437b5ab2f
ignacio <ignacioaalvarezc@gmail.com>
2023-11-20 19:09:18 -0300

    Configurando JwtToken

---
+++ src/main/java/com/sys/courses/back/infra/security/JwtRequest.java
@@ -0,0 +1,15 @@
+package com.sys.courses.back.infra.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class JwtRequest {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/sys/courses/back/infra/security/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.sys.courses.back.infra.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class JwtResponse {
+
+    private String token;
+}
