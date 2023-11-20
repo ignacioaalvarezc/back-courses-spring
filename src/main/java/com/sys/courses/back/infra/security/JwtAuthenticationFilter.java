0811a342e1ac7dd43deb7ac6040051d01f368b2e
ignacio <ignacioaalvarezc@gmail.com>
2023-11-20 19:27:06 -0300

    Configuracion de authentication filter

---
+++ src/main/java/com/sys/courses/back/infra/security/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.sys.courses.back.infra.security;
+
+import com.sys.courses.back.services.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
