9cca54583ec8e78a4539eddb8beddeb650f58383
ignacio <ignacioaalvarezc@gmail.com>
2023-11-21 12:39:54 -0300

    Configuracion del jwt authentication filter

--- src/main/java/com/sys/courses/back/infra/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sys/courses/back/infra/security/JwtAuthenticationFilter.java
@@ -1,7 +1,12 @@
 package com.sys.courses.back.infra.security;
 
 import com.sys.courses.back.services.impl.UserDetailsServiceImpl;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,7 +26,33 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtUtils jwtUtils;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String requestTokenHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwtToken = null;
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
 
+            try {
+                username = this.jwtUtils.extractUsername(jwtToken);
+            } catch (ExpiredJwtException expiredJwtException) {
+                System.out.println("El token ha espirado");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Token inválido");
+        }
+        if (username != null && SecurityContextHolder.getContext()
+                .getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if (this.jwtUtils.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            }
+        }
     }
 }
