fb6c615f20c5a51be765831197ae19984ed17889
ignacio <ignacioaalvarezc@gmail.com>
2023-11-23 09:11:17 -0300

    Metodo que genera el token y permite la autentication

---
+++ src/main/java/com/sys/courses/back/controllers/AuthenticationController.java
@@ -0,0 +1,54 @@
+package com.sys.courses.back.controllers;
+
+import com.sys.courses.back.infra.security.JwtToken.JwtRequest;
+import com.sys.courses.back.infra.security.JwtToken.JwtResponse;
+import com.sys.courses.back.infra.security.JwtToken.JwtUtils;
+import com.sys.courses.back.services.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/generate-token")
+    public ResponseEntity<?> generarToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        try {
+            authenticate(jwtRequest.getUsername(), jwtRequest.getPassword());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("Usuario no encontrado");
+        }
+
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+        String token = this.jwtUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception{
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch(DisabledException disabledException) {
+            throw new Exception("Usuario deshabilidato" + disabledException.getMessage());
+        } catch(BadCredentialsException badCredentialsException) {
+            throw new Exception ("Credenciales inválidas" + badCredentialsException.getMessage());
+        }
+    }
+}
