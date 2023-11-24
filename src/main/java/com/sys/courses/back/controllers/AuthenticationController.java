0c9c47a74a5804ff790d44d04e98e63ada19451f
ignacio <ignacioaalvarezc@gmail.com>
2023-11-24 09:39:28 -0300

    Agregando Cross Origin

--- src/main/java/com/sys/courses/back/controllers/AuthenticationController.java
+++ src/main/java/com/sys/courses/back/controllers/AuthenticationController.java
@@ -13,11 +13,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class AuthenticationController {
 
     @Autowired
