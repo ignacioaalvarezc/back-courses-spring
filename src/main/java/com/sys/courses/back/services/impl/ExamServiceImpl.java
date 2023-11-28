0019dae83e09bceb5e5cdac495f36140490c86f3
ignacio <ignacioaalvarezc@gmail.com>
2023-11-28 17:17:27 -0300

    Creacion de la implementacion del servicio de examen

---
+++ src/main/java/com/sys/courses/back/services/impl/ExamServiceImpl.java
@@ -0,0 +1,44 @@
+package com.sys.courses.back.services.impl;
+
+import com.sys.courses.back.models.Exam;
+import com.sys.courses.back.repositories.ExamRepository;
+import com.sys.courses.back.services.ExamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Service
+public class ExamServiceImpl implements ExamService {
+
+    @Autowired
+    private ExamRepository examRepository;
+
+    @Override
+    public Exam addExam(Exam exam) {
+        return examRepository.save(exam);
+    }
+
+    @Override
+    public Exam updateExam(Exam exam) {
+        return examRepository.save(exam);
+    }
+
+    @Override
+    public Set<Exam> getExams() {
+        return new LinkedHashSet<>(examRepository.findAll());
+    }
+
+    @Override
+    public Exam getExam(Long examId) {
+        return examRepository.findById(examId).get();
+    }
+
+    @Override
+    public void deleteExam(Long examId) {
+        Exam exam = new Exam();
+        exam.setExamId(examId);
+        examRepository.delete(exam);
+    }
+}
