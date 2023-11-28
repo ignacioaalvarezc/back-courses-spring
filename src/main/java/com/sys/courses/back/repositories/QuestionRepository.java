bb6d7e91a7d1eedaf9290640359fa503af7285f2
ignacio <ignacioaalvarezc@gmail.com>
2023-11-28 17:05:12 -0300

    Configuracion repositorio de preguntas

--- src/main/java/com/sys/courses/back/repositories/QuestionRepository.java
+++ src/main/java/com/sys/courses/back/repositories/QuestionRepository.java
@@ -1,7 +1,11 @@
 package com.sys.courses.back.repositories;
 
+import com.sys.courses.back.models.Exam;
 import com.sys.courses.back.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Set<Question> findByExam(Exam exam);
 }
