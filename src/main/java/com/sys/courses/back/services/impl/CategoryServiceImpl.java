34810fdcb8b55c54fcd81f32abb3fc8ee529d407
ignacio <ignacioaalvarezc@gmail.com>
2023-11-29 10:12:37 -0300

    Implementacion de metodos en la implementacion del servicio de categorias

--- src/main/java/com/sys/courses/back/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/sys/courses/back/services/impl/CategoryServiceImpl.java
@@ -1,4 +1,44 @@
 package com.sys.courses.back.services.impl;
 
-public class CategoryServiceImpl {
+import com.sys.courses.back.models.Category;
+import com.sys.courses.back.repositories.CategoryRepository;
+import com.sys.courses.back.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Set<Category> getCategories() {
+        return new LinkedHashSet<>(categoryRepository.findAll());
+    }
+
+    @Override
+    public Category getCategory(Long categoryId) {
+        return categoryRepository.findById(categoryId).get();
+    }
+
+    @Override
+    public void deleteCategory(Long categoryId) {
+        Category category = new Category();
+        category.setCategoryId(categoryId);
+        categoryRepository.delete(category);
+    }
 }
