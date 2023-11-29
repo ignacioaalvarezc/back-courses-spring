82cb000e9a5ffc4313c869c1cd40fae6fb821837
ignacio <ignacioaalvarezc@gmail.com>
2023-11-29 10:19:18 -0300

    Implementacion de metodos en la implementacion del servicio de cursos

--- src/main/java/com/sys/courses/back/services/impl/CourseServiceImpl.java
+++ src/main/java/com/sys/courses/back/services/impl/CourseServiceImpl.java
@@ -1,4 +1,44 @@
 package com.sys.courses.back.services.impl;
 
-public class CourseServiceImpl {
+import com.sys.courses.back.models.Course;
+import com.sys.courses.back.repositories.CourseRepository;
+import com.sys.courses.back.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Override
+    public Course addCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Course updateCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Set<Course> getCourses() {
+        return new LinkedHashSet<>(courseRepository.findAll());
+    }
+
+    @Override
+    public Course getCourse(Long courseId) {
+        return courseRepository.findById(courseId).get();
+    }
+
+    @Override
+    public void deleteCourse(Long courseId) {
+        Course course = new Course();
+        course.setCourseId(courseId);
+        courseRepository.delete(course);
+    }
 }
