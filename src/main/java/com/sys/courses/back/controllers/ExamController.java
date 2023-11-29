284693b7de0fe5d9ab15bc36eda794ec6d564730
ignacio <ignacioaalvarezc@gmail.com>
2023-11-29 11:17:55 -0300

    Creacion e implementacion de metodos en el controlador de examenes

---
+++ src/main/java/com/sys/courses/back/controllers/ExamController.java
@@ -0,0 +1,44 @@
+package com.sys.courses.back.controllers;
+
+import com.sys.courses.back.models.Category;
+import com.sys.courses.back.models.Exam;
+import com.sys.courses.back.services.CategoryService;
+import com.sys.courses.back.services.ExamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/exams")
+@CrossOrigin("*")
+public class ExamController {
+
+    @Autowired
+    private ExamService examService;
+
+    @PostMapping("/")
+    public ResponseEntity<Exam> saveExam(@RequestBody Exam exam) {
+        return ResponseEntity.ok(examService.addExam(exam));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Exam> updateExam(@RequestBody Exam exam) {
+        return ResponseEntity.ok(examService.addExam(exam));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> listExams() {
+        return ResponseEntity.ok(examService.getExams());
+    }
+
+    @GetMapping("/{examId}")
+    public Exam listExam(@PathVariable("examId") Long examId) {
+        return examService.getExam(examId);
+    }
+
+    @DeleteMapping("/{examId}")
+    public void deleteExam(@PathVariable("examId") Long examId) {
+        examService.deleteExam(examId);
+    }
+
+}
